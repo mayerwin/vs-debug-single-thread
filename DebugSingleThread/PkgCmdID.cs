f44b9d9821b6124b53fdd090d000f5f855371c19
Erwin Mayer <mayerwin@users.noreply.github.com>
2011-06-27 04:44:42 +0200

    Initial v.1.0 code release.

---
+++ DebugSingleThread/PkgCmdID.cs
@@ -0,0 +1,14 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace ErwinMayerLabs.DebugSingleThread
+{
+    static class PkgCmdIDList
+    {
+        public const uint FocusOnCurrentThreadCmd = 0x100;
+        public const uint SwitchToNextThreadCmd = 0x101;
+
+
+    };
+}
